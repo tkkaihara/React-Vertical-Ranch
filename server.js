93ef53919f2a66aa805d71aca7a66a5dee1cd35a
tkkaihara <kenzokaihara@gmail.com>
2020-08-11 15:49:10 -0700

    trobleshooting again again again

--- server.js
+++ server.js
@@ -29,12 +29,13 @@ app.use("/api/campgrounds", campgrounds);
 app.use("/api/campgrounds/:id/bookings", bookings);
 
 if (process.env.NODE_ENV === "production") {
+  // Set static folder
   app.use(express.static("client/build"));
-}
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
