ed449563a80e93804c20e00b8edd85a5a94102a1
tkkaihara <kenzokaihara@gmail.com>
2020-08-11 15:58:22 -0700

    troubleshooting some more

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use("/api/campgrounds/:id/bookings", bookings);
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use("/", express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "/client/build", "index.html"));
   });
 }
 
