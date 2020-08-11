553505cb69bd570c5e3345fe29bc916540d0924e
tkkaihara <kenzokaihara@gmail.com>
2020-08-11 16:59:37 -0700

    Fix unexpected token <

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use("/api/campgrounds/:id/bookings", bookings);
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("/client/build"));
+  app.use("/", express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
